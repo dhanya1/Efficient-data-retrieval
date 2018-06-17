4acc492f6eab165f7436b6797ee1ad1697ae7859
Dhanya Jayachandra <dhanyasj01@gmail.com>
2018-06-17 14:15:39 +0100

    Bad URL checks

--- app.py
+++ app.py
@@ -3,69 +3,65 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
 from sqlalchemy import Column, Integer
 import task
+import datetime
 
 app = Flask(__name__)
 
-class events(object):
-    pass
+def validate_request(**args):
+    try:
+        a = int(args['device_id'])
+        b = int(args['content_id'])
+    except:
+        error = 'BadURL: Device and Content ID must be an integer'
+    return error
 
-class persons(object):
-    pass
+def function(**args):
+    args = request.args.to_dict()
+    error = validate_request(args)
+    if not error:
+        try:
+            result = task.server_requests(**args)
+        except:
+            error = 'Error occured'
+    return result,error
 
-def load_session():
-    engine = create_engine('sqlite:////users/mscdsa2018/dsj1/advertima/advertima.db', echo=True)
-    metadata = MetaData(engine)
-    event = Table('event', metadata, Column("event_id", Integer, primary_key=True), autoload=True)
-    mapper(events, event)
-    #persons = Table('person', metadata, autoload = True)
-    #mapper(persons, person)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
-
-'''
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content_ID = db.Column(db.String(100))
-    device_ID = db.Column(db.String(100))
-    start_time = db.Column(db.Text(100))
-    end_time = db.Column(db.Text(100))
-
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    device_id = db.Column(db.String(100))
-    appears = db.Column(db.Text(100))
-    disappears = db.Column(db.Text(100))
-    age = db.Column(db.String(100))
-    gender = db.Column(db.String(100))
-'''
-#db.create_all()
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Advertima API Home Page"
 
 @app.route("/viewer-count",methods=['GET'])
 def views():
     args = request.args.to_dict()
     args['request'] = 'view'
-    args['view'] = task.server_requests(**args)
+    (result,error) = function(args)
+    if error: 
+        args['error'] = error
+    else:
+        args['view'] = result
     return jsonify(args)
 
 @app.route("/avg-age", methods=['GET'])
 def avg_age():
     args = request.args.to_dict()
     args['request'] = 'avg_age'
-    args['avg_age'] = task.server_requests(**args)
-    return jsonify(args)
+    (result,error) = function(args)
+    if error: 
+        args['error'] = error
+    else:
+        args['avg_age'] = result
+    return jsonify(args) 
 
 
 @app.route("/gender-dist", methods=['GET'])
 def getall_events():
     args = request.args.to_dict()
     args['request'] = 'gender_dist'
-    args['gender_dist'] = task.server_requests(**args)
+    (result,error) = function(args)
+    if error:   
+        args['error'] = error
+    else:
+        args['gender_dist'] = result
     return jsonify(args)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
